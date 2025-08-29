9ce21f4c5d64c42456173a4e5aa188f6f23da976
Tempus23 <carlos.cbm32@gmail.com>
2025-08-29 20:47:46 +0200

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import robotsTxt from "astro-robots-txt";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: "https://tempus23.github.io",
-  base: "/mi-portfolio",
+  // QUITA base y site para Pages en raíz
+  // output por defecto ya es "static" en tu build
 });
