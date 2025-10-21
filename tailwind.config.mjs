ed93ecdaf6a4a40e4524a6831c216cf192165070
Carlos HM <cchernan@mercadona.es>
2025-10-21 22:07:46 +0200

    feat: no se que hago

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+	export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#6366F1', // Indigo 500
+				secondary: '#8B5CF6', // Violet 500
+				dark: '#1A202C', // Dark Gray
+				light: '#F7FAFC', // Light Gray
+			},
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
